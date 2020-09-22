0946bb3af1c565e33dbd9b25765b0c7fafdb0053
Daniel Ramírez Morilla <drami@Kioto>
2020-09-22 20:23:29 +0200

    Prueba de que Git funciona junto a Eclipse.

--- pruebaGit/src/pruebaGit/helloWorld.java
+++ pruebaGit/src/pruebaGit/helloWorld.java
@@ -4,7 +4,7 @@ public class helloWorld {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hoola buenas tardes mire quería pedir una picsa.");
+		System.out.println("Hoola buenas tardes mire quería pedir una picssa.");
 
 	}
 
