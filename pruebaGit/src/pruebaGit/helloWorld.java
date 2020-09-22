cb130418e523fe071ca5d1aa033a36e71e8025dd
Daniel Ramírez Morilla <daniel@xarblanca.es>
2020-09-22 21:53:37 +0200

    Prueba usuario.

--- pruebaGit/src/pruebaGit/helloWorld.java
+++ pruebaGit/src/pruebaGit/helloWorld.java
@@ -4,7 +4,7 @@ public class helloWorld {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hoola buenas tardes mire quería pedir una picssa.");
+		System.out.println("Esto está relindo vihte");
 
 	}
 
